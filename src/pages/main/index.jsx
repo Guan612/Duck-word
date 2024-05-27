747832bab0a766fa7153858622f1e8ff41f95d3d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-27 12:36:23 +0800

    添加路由标签

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,8 +1,9 @@
+import { NavLink } from "react-router-dom";
 import { TodoListO } from "@react-vant/icons";
 export default function Main() {
     return (
         <div
-            className="felx  items-center h-full bg-cover bg-center"
+            className="felx items-center h-full bg-cover bg-center"
             style={{ backgroundImage: 'url("https://bing.img.run/m.php")' }}
         >
             <div className="flex flex-col justify-center items-center h-3/5">
@@ -12,12 +13,12 @@ export default function Main() {
                 </div>
             </div>
             <div className="flex flex-row justify-around items-center h-1/5 m-2">
-                <div className="w-1/2 items-center rounded-2xl px-16 py-6 backdrop-filter backdrop-blur-lg">
+                <NavLink to={"/learn/learnword"} className="w-1/2 items-center rounded-2xl px-16 py-6 backdrop-filter backdrop-blur-lg">
                     <div className="text-lg font-bold text-white mr-4">
                         Learn
                     </div>
                     <div className="text-violet-700">160</div>
-                </div>
+                </NavLink>
                 <div className="m-1"></div>
                 <div className="w-1/2 items-center rounded-2xl px-16 py-6 backdrop-filter backdrop-blur-lg">
                     <div className="text-lg font-bold text-white mr-4">
