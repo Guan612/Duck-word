8fd60634abdd7a9d58e294f75c755ad59ace9c92
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-27 11:11:39 +0800

    基础框架搭建

--- src/pages/me/index.jsx
+++ src/pages/me/index.jsx
@@ -1,6 +1,6 @@
 export default function Me() {
     return (
-        <div>
+        <div className="flex flex-col">
             <h1>我的</h1>
         </div>
     );
--- src/pages/wordtext/index.jsx
+++ src/pages/wordtext/index.jsx
@@ -1,7 +1,15 @@
 export default function WordText() {
     return (
-        <div>
-            <h1>WordText</h1>
+        <div className="flex flex-col justify-center items-center">
+            <div>
+                已学单词
+            </div>
+            <div>
+                生词
+            </div>
+            <div>
+                收藏词汇
+            </div>
         </div>
     )
 }
