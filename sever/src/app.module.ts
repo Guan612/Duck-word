120333049356f12cdef45edb1295c33d6df8c5a9
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-31 22:18:57 +0800

    添加模块

--- sever/src/app.module.ts
+++ sever/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { DbService } from './db/db.service';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, DbModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DbService,],
 })
 export class AppModule {}
