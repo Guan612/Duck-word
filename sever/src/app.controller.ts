268e5579f040ec2a1158e15333368bf80a6ff506
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-31 15:57:27 +0800

    实验swagger

--- sever/src/app.controller.ts
+++ sever/src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ApiBearerAuth,
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('主入口')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Create cat' }) //描述作用
+  @ApiResponse({ status: 200, description: '响应成功'})//描述响应码
   getHello(): string {
     return this.appService.getHello();
   }
