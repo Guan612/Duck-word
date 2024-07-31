b5f19314f8807b68c2f1e6cd5d95638ca9e69143
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-07-31 22:20:53 +0800

    用户模块

--- sever/src/users/users.controller.ts
+++ sever/src/users/users.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
 
+@ApiTags('用户')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @ApiOperation({ summary: '创建用户' })
+  //@ApiResponse({ status: 200, description: '响应成功' }) //描述响应码
+  create(@Body() createUserDto:Prisma.UserCreateManyInput) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: '查找所有用户' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '查找指定用户' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @ApiOperation({ summary: '更新指定用户' })
+  update(@Param('id') id: string, @Body() updateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: '删除指定用户' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
--- sever/src/users/users.service.ts
+++ sever/src/users/users.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User,Prisma } from '@prisma/client';
+import { DbService } from 'src/db/db.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: DbService) {}
+
+  async create(createUserDto: Prisma.UserCreateInput) {
+    const res = await this.prisma.user.create({
+      data:createUserDto
+    })
+    return {
+      code:0,
+      msg:'创建用户成功',
+      res:{
+        username:res.loginId,
+      }
+    }
   }
 
   findAll() {
@@ -16,7 +27,7 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: number, updateUserDto) {
     return `This action updates a #${id} user`;
   }
 
